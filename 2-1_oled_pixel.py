e9bf522366c87e8e4efc44f14639c5c11c515d47
chyijiunn <chyijiunn@users.noreply.github.com>
2024-07-14 17:08:39 +0800

    ud

--- 2-1_oled_pixel.py
+++ 2-1_oled_pixel.py
@@ -2,7 +2,7 @@ from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
 import time
 
-i2c=I2C(0,sda=Pin(20), scl=Pin(21), freq=40000)
+i2c=I2C(1,sda=Pin(20), scl=Pin(21), freq=40000)
 oled = SSD1306_I2C(128, 64, i2c)
 
 oled.fill(0)
--- 2-2_oled_random.py
+++ 2-2_oled_random.py
@@ -1,6 +1,6 @@
 from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
-import time,random
+import time
 
 i2c=I2C(0,sda=Pin(20), scl=Pin(21), freq=40000)
 oled = SSD1306_I2C(128, 64, i2c)
